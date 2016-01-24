75f8748ee8c92fae6df40407e4b84755949a74ff
Blake Garretson <blake.garretson@gmail.com>
2016-01-23 20:05:29 -0500

    Prob7 file actually contained Prob10 solution (which was missing).  Renamed and reimplemented Prob7.  Kind of kludgy.

--- prob7.py
+++ prob7.py
@@ -1,16 +1,15 @@
 import math
 
-primes = []
-
-n=2
-while n < 2e6:
+primenum = 1
+n=1
+while primenum <= 10001:
+    n += 1
     isprime = True
     for x in range(2,int(math.sqrt(n))+1) :
         if n % x == 0:
             isprime = False
             break
     if isprime:
-        primes.append(n)
-    n += 1
+        primenum += 1
     
-print sum(primes)
+print(n)
