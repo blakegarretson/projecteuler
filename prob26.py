002ffe89b978b1bdef0112c85b877301fbaa8f03
Blake Garretson <blake.garretson@gmail.com>
2017-04-13 12:30:47 -0400

    prob26 works

--- prob26.py
+++ prob26.py
@@ -1,22 +1,24 @@
-d=1
-digits=[]
-while d < 1000:
-    digits.append((d,str(1.0/d)[2:]))
-    d+=1
- 
-ld=0
-l=0
+import decimal
+decimal.getcontext().prec=10000
 
-for d,n in digits:
-    f=n[0]
-    sl=f
-    for x in n[1:]:
-        sl+=sl
-        if x==f:
-            if len(sl)>l:
-                ld=d
-                l=len(sl)
-                
-print(ld,l)
-        
-        
+longest_num=0
+longest=0
+
+n=1
+while n < 1000:
+    #print('working on ', n)
+    digits = str(decimal.Decimal(1.0) /
+            decimal.Decimal(n))[2:]
+    if len(digits) > 1: 
+        for x in range(2, 5001):
+            if digits[x:].startswith(
+                        digits[:x]):
+                #print(n,x,digits)
+                if x > longest:
+                    longest_num = n
+                    longest = x
+                break
+    n += 1
+   
+print('------------')                
+print('num: ', longest_num,'\nlength: ', longest)
